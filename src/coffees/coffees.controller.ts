0da14bccf5988c3a5756191a35b9d9445fb5bf88
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2023-12-06 18:13:53 +0200

    handle update and delete requests

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,19 +1,20 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
   Param,
+  Patch,
   Post,
-  Res,
 } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
-  findAll(@Res() response) {
-    response.status(200).send('This action returns all coffees');
+  findAll() {
+    return 'This action returns all coffees';
   }
   @Get(':id')
   findOne(@Param('id') id: string) {
@@ -23,5 +24,13 @@ export class CoffeesController {
   @HttpCode(HttpStatus.GONE)
   create(@Body() body) {
     return body;
+  }
+  @Patch(':id')
+  update(@Param('id') id: string) {
+    return `This action updates #${id} coffee`;
+  }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee`;
   }
 }
