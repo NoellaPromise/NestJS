c7917b4916b2a3f817558c2107940a038ffd9cbf
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2023-12-06 17:44:49 +0200

    respond with status code

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,16 +1,26 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-  @Get('flavors')
-  findAll() {
-    return 'This action returns all coffees';
+  @Get()
+  findAll(@Res() response) {
+    response.status(200).send('This action returns all coffees');
   }
   @Get(':id')
-  findone(@Param('id') id: string) {
+  findOne(@Param('id') id: string) {
     return `This action returns #${id} coffee`;
   }
   @Post()
+  @HttpCode(HttpStatus.GONE)
   create(@Body() body) {
     return body;
   }
